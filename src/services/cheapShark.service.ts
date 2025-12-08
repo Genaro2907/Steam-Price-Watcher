e33aad586336f98c4101c2f9fef9a2d7f03da801
Gabriel Genaro <g.genaro.contato@gmail.com>
2025-12-08 16:37:57 -0300

    Steam Service

---
+++ src/services/cheapShark.service.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosInstance } from "axios";
+import z from "zod";
+
+const CheapSharkGameSchema = z.object({
+     gemeID: z.string(),
+     external: z.string(),
+     cheapest: z.string(),
+     thumb: z.string().optional(),   
+});
+
+export type CheapSharkGameResponse = z.infer<typeof CheapSharkGameSchema>;
+
+
+export class CheapSharkService {
+    private http: AxiosInstance;
+
+    constructor() {
+        this.http = axios.create({
+            baseURL: 'httos://www.cheapshark.com/api/1.0',
+            timeout: 5000
+        });
+    }
+
+    async searchGames(title: string): Promise<CheapSharkGameResponse[]> {
+        try {
+
+            const response = await this.http.get('/games', {
+                params: {
+                    title: title,
+                    limit: 10
+                }
+            });
+
+            const parsedData = z.array(CheapSharkGameSchema).safeParse(response.data);
+            if(!parsedData.success) {
+                console.error("Erro de validação da API externa:", parsedData.error);
+                return [];
+            }
+
+            return parsedData.data; 
+        } catch (error) {
+            console.error("Erro ao conectar com CheapShark:", error);
+            return [];
+        }
+    }
+}
+
+export const cheapSharkService = new CheapSharkService();
---
+++ src/services/steam.service.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosInstance } from "axios";
+import z from "zod";
+
+const SteamPriceSchema = z.object({
+    price_overvies: z.object({
+        currency: z.string(),
+        initial: z.number(),
+        final: z.number(),
+        discount_percent: z.number(),
+        final_formatted: z.string()
+    }).optional()
+});
+
+
+export class SteamService {
+    private readonly BASE_URL  = 'https://store.steampowered.com/api/appdetails';
+
+    async getGamePriceInBRL(steamAppId: string) {
+        try {
+            const response = await axios.get(this.BASE_URL, {
+                params: {
+                    appids: steamAppId,
+                    cc: 'br',
+                    filters: 'price_overview'
+                }
+            })
+
+            const gameData = response.data[steamAppId];
+
+            if(!gameData || !gameData.success) {
+                console.warn(`[SteamService] Falha ao obter dados para ID ${steamAppId}`);
+                return null;
+            }
+            const parsedData = SteamPriceSchema.safeParse(gameData.data);
+
+            if(!parsedData.success){
+                return null;
+            }
+
+            return parsedData.data;
+        } catch (error) {
+            console.error(`[SteamService] Erro na requisição:`, error);
+            return null;
+        }
+    }
+}
+
+export const steamService = new SteamService();
